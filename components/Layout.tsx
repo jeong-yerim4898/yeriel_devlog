93ff5fda9f327cea7cedc0ffa8ce262148c73920
“jeong-yerim4898” <jinju151002@gmail.com>
2023-07-20 00:57:06 +0900

    edit children type

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,8 @@
 import Header from "./Header";
+import React, { ReactNode } from "react";
 
 
-export default function Layout({children}) {
+export default function Layout({children}:any) {
     return (
         <>
             <Header/>
