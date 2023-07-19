6f08df62667dc93c1ec95259c3556c79c061c20f
“jeong-yerim4898” <jinju151002@gmail.com>
2023-07-20 00:47:06 +0900

    delete Image components

--- components/Image.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-const customLoader = ({ src }) => {
-    return src
-}
-export default function Img(props) {
-    return (<Image {...props} loader={customLoader}/>);
-}
