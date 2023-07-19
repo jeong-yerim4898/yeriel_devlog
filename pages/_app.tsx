d911a3d2e4131c9da5b57a92b575f42b3f6ca476
“Yerim <jinju151002@gmail.com>
2023-07-20 01:03:37 +0900

    glass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import Layout from "../components/Layout";
 import './global.scss';
 
 export default function MyApp({ Component, pageProps }:any) {
-    console.log(Component)
     return (
         <Layout>
             <Component {...pageProps}/>
