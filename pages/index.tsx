e1fc4f58dd0534a601d4763f48b9f4d3e0c2b3cc
jeong-yerim4898 <jinju151002@gmail.com>
2023-07-21 23:36:38 +0900

    add education posting

--- pages/index.tsx
+++ pages/index.tsx
@@ -75,7 +75,18 @@ const EmphasizeWord = styled('span',{
     fontWeight:'bold',
 });
 
-const ExperienceSection = styled('section',{
+const ExperienceTech = styled('div',{
+    display:'flex',
+    gap:'.25rem',
+    paddingBottom:'.25rem',
+    '>span':{
+        backgroundColor:'$lightGray02',
+        color:'$blackDark40',
+        borderRadius:'.25rem',
+        padding:'.125rem .3rem',
+        display: 'inline-block',
+        fontSize:'0.875rem'
+    }
 });
 
 const ExperienceUl = styled('ul',{
@@ -92,7 +103,7 @@ const ExperienceUl = styled('ul',{
 export default function Home() {
 
     const renderIntroduce = () => {
-        return (<IntroduceSection>
+        return (<IntroduceSection id={'introduce'}>
             <SectionTitle>Introduce.</SectionTitle>
 
             <IntroduceContent>{`안녕하세요. 공감능력으로 요구사항을 놓치지 않는 프론트엔드 개발자 정예림입니다.\n
@@ -106,7 +117,7 @@ React, Next.js, scss, Typescript, HTML 등을 사용하여 `}
     };
 
     const renderExperience = () => {
-        return (<ExperienceSection>
+        return (<section id={'experience'}>
                 <SectionTitle>Experience.</SectionTitle>
                 <div style={{display:'flex'}}>
                     <Image src={'/igaworks.webp'} alt={'아이지에이웍스 로고'} width={200} height={200}
@@ -114,12 +125,18 @@ React, Next.js, scss, Typescript, HTML 등을 사용하여 `}
                     <div style={{marginLeft:'1.5rem'}}>
                         <div>
                             <h2 style={{fontWeight:"bold",fontSize:'2rem'}}>아이지에이웍스</h2>
-                            <p style={{color:COLOR.lightGray03,paddingBottom:".5rem"}}>MobileIndex 개발실 솔루션팀 프론트엔드 개발자</p>
+                            <p style={{color:COLOR.lightGray03,paddingBottom:".5rem"}}>MI 개발실 솔루션팀 프론트엔드 개발자</p>
                             <div style={{color:COLOR.blackDark70}}>빅데터 플랫폼기업 아이지에이웍스에서 DMP기반 앱 분석 서비스 모바일 인덱스, 원스톱 디지털 전환 서비스 마케팅클라우드를 개발하는 솔루션 개발팀에서 프론트엔드 개발자로 근무하고 있습니다.
                                 데이터 시각화에 필요한 차트, 다양한 컴포넌트, 랜딩페이지,팀 기술블로그 등 다양한 웹 서비스를 만들고 유지보수 하고 있습니다.</div>
                         </div>
                         <div>
                             <h4 style={{display:'block',fontWeight:"bold",fontSize:'1.25rem',marginTop:"1.5rem",marginBottom:'0.8125rem'}}>ZTL (23.05 ~ 23.06)</h4>
+                            <ExperienceTech>
+                                <span>React</span>
+                                <span>Typescript</span>
+                                <span>MobX</span>
+                                <span>SCSS</span>
+                            </ExperienceTech>
                             <ExperienceUl style={{marginLeft:'2rem'}}>
                                 <li>기존의 컴포넌트 성능개선 (셀렉트, 체크박스,스위치, 데이트피커 등) 맟 CRUD 페이지 개발</li>
                                 <li>업종 선택필터 개발 및 API 연결</li>
@@ -127,8 +144,15 @@ React, Next.js, scss, Typescript, HTML 등을 사용하여 `}
                         </div>
                         <div>
                             <h4 style={{display:'block',fontWeight:"bold",fontSize:'1.25rem',marginTop:"1.5rem",marginBottom:'0.8125rem'}}>사내 개발블로그 개발 (23.03 ~ 23.07)</h4>
+                            <ExperienceTech>
+                                <span>Next.js</span>
+                                <span>Typescript</span>
+                                <span>MobX</span>
+                                <span>SCSS</span>
+                                <span>AWS</span>
+                            </ExperienceTech>
+                            <a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://devlog.mi.igaw.io/'}><ResumeLink type={"service"}>https://devlog.mi.igaw.io/</ResumeLink></a>
                             <ExperienceUl style={{marginLeft:'2rem'}}>
-                                <a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://devlog.mi.igaw.io/'}><ResumeLink type={"service"}>https://devlog.mi.igaw.io/</ResumeLink></a>
                                 <li>Next.js를 활용한 사내 개발블로그 개발 (디자인, 개발 모두 수행)</li>
                                 <li>AWS CodePiple을 통해 CI/CD 구성</li>
                                 <li>포스팅 CRUD에 필요한 기술 개발(마크다운, 이미지 업로드, 개인 URI 설정 등)</li>
@@ -137,34 +161,80 @@ React, Next.js, scss, Typescript, HTML 등을 사용하여 `}
                         </div>
                         <div>
                             <h4 style={{display:'block',fontWeight:"bold",fontSize:'1.25rem',marginTop:"1.5rem",marginBottom:'0.8125rem'}}>2022 연말결산 랜딩페이지 개발 (23.01)</h4>
+                            <ExperienceTech>
+                                <span>React</span>
+                                <span>Typescript</span>
+                                <span>MobX</span>
+                                <span>SCSS</span>
+                                <span>AWS</span>
+                            </ExperienceTech>
+                            <a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://landscape.mktcloud.igaworks.com/'}><ResumeLink type={"service"}>https://landscape.mktcloud.igaworks.com/</ResumeLink></a>
                             <ExperienceUl style={{marginLeft:'2rem'}}>
-                                <a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://landscape.mktcloud.igaworks.com/'}><ResumeLink type={"service"}>https://landscape.mktcloud.igaworks.com/</ResumeLink></a>
                                 <li><a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://velog.io/@wjd489898/%ED%9B%84%EA%B8%B0-2022-%EB%9E%9C%EB%93%9C%EC%8A%A4%EC%BC%80%EC%9D%B4%ED%94%84-%EA%B0%9C%EB%B0%9C'}><ResumeLink>다양한 애니메이션이 담긴 랜딩페이지</ResumeLink></a>를 통한 통한 사내 서비스 가입 유도</li>
                                 <li>AWS ESC와 CodePipline을 통한 CI/CD 구성</li>
                                 <li>반응형 디자인 적용</li>
                             </ExperienceUl>
                         </div>
                         <div>
-                            <h4 style={{display:'block',fontWeight:"bold",fontSize:'1.25rem',marginTop:"1.5rem",marginBottom:'0.8125rem'}}>서비스 디자인 시스템 및 차트 개발</h4>
+                            <h4 style={{display:'block',fontWeight:"bold",fontSize:'1.25rem',marginTop:"1.5rem",marginBottom:'0.8125rem'}}>다양한 개발들...</h4>
+                            <ExperienceTech>
+                                <span>React</span>
+                                <span>Typescript</span>
+                                <span>MobX</span>
+                                <span>SCSS</span>
+                                <span>SVG</span>
+                                <span>Node.js</span>
+                                <span>MongoDB</span>
+                            </ExperienceTech>
                             <ExperienceUl style={{marginLeft:'2rem'}}>
                                 <li>사내 디자인 시스템 개발</li>
-                                <li>SVG를 활용한 파이차트, 막대차트, 워드클라우드, 지도 차트 개발 (2022)</li>
-                                <li><a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://velog.io/@wjd489898/%EC%84%B1%EB%8A%A5%EC%9D%84-%EA%B0%9C%EC%84%A0%ED%95%9C-%EC%9B%8C%EB%93%9C%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-Web-Worker'}><ResumeLink>worker를 사용한 워드클라우드 성능개선</ResumeLink></a></li>
-                            </ExperienceUl>
-                        </div>
-                        <div>
-                            <h4 style={{display:'block',fontWeight:"bold",fontSize:'1.25rem',marginTop:"1.5rem",marginBottom:'0.8125rem'}}>기타 사내 서비스</h4>
-                            <ExperienceUl style={{marginLeft:'2rem'}}>
+                                <li>SVG를 활용한 파이차트, 막대차트, 워드클라우드, 지도 차트 개발</li>
                                 <li>메타게임 서비스 Node.js, MongoDB를 통한 백엔드 개발 </li>
-                                <li>SVG와 키보드 이벤트 계산을 통해 지렁이 게임 개발 (2021)</li>
                                 <li>사내 개인정보처리방침 페이지 개발 및 업데이트</li>
-                                <li><a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://miki.igaw.io/docs'}><ResumeLink>팀 문서서비스(MIKi)</ResumeLink></a> 다자인 변경 반영</li>
                             </ExperienceUl>
                         </div>
                     </div>
                 </div>
+            </section>
+        )
+    };
 
-            </ExperienceSection>
+    const renderEducation = () => {
+        return (<section id={'education'} style={{width:'100%'}}>
+            <SectionTitle>Education.</SectionTitle>
+            <div className={'education-wrapper'} style={{paddingLeft:'12.5rem'}}>
+                <div style={{marginBottom:'2rem'}}>
+                    <h2 style={{fontWeight:"bold",fontSize:'2rem',paddingBottom:'.25rem'}}>SSAFY 4기</h2>
+                    <p style={{color:COLOR.blackDark40,paddingBottom:'.25rem'}}>2020.07 ~ 2021.07</p>
+                    <p style={{color:COLOR.blackDark92}}>삼성청년 SW 아카데미 4기 수료</p>
+                </div>
+                <div>
+                    <h2 style={{fontWeight:"bold",fontSize:'2rem',paddingBottom:'.25rem'}}>금오공과대학교</h2>
+                    <p style={{color:COLOR.blackDark40,paddingBottom:'.25rem'}}>2015.03 ~ 2020.02</p>
+                    <p style={{color:COLOR.blackDark92}}>산업경영공학 전공 & 교내 비전공자 SW교육 과정 1등 수료(2017)</p>
+                </div>
+            </div>
+
+        </section>)
+    }
+
+    const renderPosting = () => {
+        return (<section id={'posting'} style={{width:'100%'}}>
+                <SectionTitle>Posting.</SectionTitle>
+                    <ul style={{paddingLeft:'12.5rem'}}>
+                        <li>
+                            <span style={{fontWeight:'bold'}}>2023.07 </span>
+                            <a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://devlog.mi.igaw.io/component-date-picker'}><ResumeLink>공공데이터를 활용한 Calendar/DatePicker 구현: 컴포넌트 편
+                            </ResumeLink></a>
+                        </li>
+                        <li><span style={{fontWeight:'bold'}}>2023.05 </span>
+                            <a style={{textDecoration:"inherit"}} target={'_blank'} href={'https://devlog.mi.igaw.io/performance-improvement-wordCloud'}><ResumeLink>내가 만든 워드클라우드 : 성능개선을 위한 여정 (Web Worker)
+                            </ResumeLink></a>
+                        </li>
+                    </ul>
+
+
+            </section>
         )
     }
     return (
@@ -185,6 +255,7 @@ React, Next.js, scss, Typescript, HTML 등을 사용하여 `}
                     </div>
                     <div>
                         <ProfileCategory>GitHub</ProfileCategory>
+                        <a href={'https://github.com/jeong-yerim4898'} target={"_blank"} style={{textDecoration:"inherit"}}></a>
                         <a href={'https://github.com/jeong-yerim4898'} target={"_blank"} style={{textDecoration:"inherit"}}>
                             <ResumeLink>https://github.com/jeong-yerim4898</ResumeLink></a>
                     </div>
@@ -192,7 +263,8 @@ React, Next.js, scss, Typescript, HTML 등을 사용하여 `}
             </ProfileSection>
             {renderIntroduce()}
             {renderExperience()}
-
+            {renderEducation()}
+            {renderPosting()}
         </MainContainer>
     );
 }
